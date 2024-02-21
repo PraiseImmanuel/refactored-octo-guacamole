e40c7485df88f8624de08ef7c67b58ab980388d6
PraiseImmanuel <praiseimmanuel601@gmail.com>
2024-02-21 08:09:22 +0100

    new: Refactoring Pages

--- src/features/layout/index.tsx
+++ src/features/layout/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import Navbar from "./navbar";
+// import Navbar from "./navbar";
 import Footer from "./components/footer";
 import Modal from "./components/modal";
 import { AnimatePresence } from "framer-motion";
@@ -13,7 +13,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [isModalOpen, setModalOpen] = useState(false);
   return (
     <div className="font-poppins ">
-      <Navbar setModalOpen={setModalOpen} />
+      {/* <Navbar setModalOpen={setModalOpen} /> */}
       <div className="pt-[84px] md:pt-[100px] min-h-[calc(100vh-70px)]">
         {children}
       </div>
